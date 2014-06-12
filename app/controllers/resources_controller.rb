75469c145692a6c413122f507e5fe9c5ad24fc08
Brittan McGinnis <konamax123@gmail.com>
2014-06-12 16:51:42 -0700

    able to create a new resource

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,7 +1,14 @@
 class ResourcesController < ApplicationController
 
   def index
-    @resources = Resource.all 
+    @category = Category.find(params[:category_id])
+    @resource = Resource.new
+    @resources = @category.resources.all 
+  end
+
+  def create
+    @resource = Resource.create(name: params[:resource][:name], description: params[:resource][:description], url: params[:resource][:url], category_id: params[:category_id])
+    redirect_to action: "index"
   end
 
 end
