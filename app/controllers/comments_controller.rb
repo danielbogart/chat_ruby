b7b2277d38f024c9f2196809fcdc962963d2fb5f
Brittan McGinnis <konamax123@gmail.com>
2014-09-03 17:06:59 -0700

    wip

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,6 @@ class CommentsController < ApplicationController
 
   def create
     commentable = params[:commentable_type].constantize.find_by( id: params[:commentable_id] )
-    p "***********"
-    p params
-    p "***********"
     if commentable.present? && commentable.respond_to?( :comments )
       @comment = Comment.new(comment_params)
       @comment.user = @current_user
