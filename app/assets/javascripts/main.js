4bf480b357f692f3aeafc1a33593847d2df609e1
Brittan McGinnis <konamax123@gmail.com>
2014-09-03 20:46:44 -0700

    comments on resources working as expected but I removed turbolinks

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
   CommentFormRevealer.bindEvents();
+  // console.log("Yo I'm in the controller")
+  // $('.see-form').on("click", function(){
+  //   console.log("I'm here");
+  //   $('.dropdown-toggle').hide().removeClass('hide').slideDown('fast');
+  // });
   
-})
+});
