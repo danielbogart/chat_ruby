602554f9c57d5d32f0a4cd1b6ffaabd7e29396f9
Brittan McGinnis <konamax123@gmail.com>
2014-09-30 20:01:35 -0700

    added devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.user = @current_user
+    @post.user = current_user
 
     if @post.save
       flash[:notice] = "#{@post.title} saved."
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,54 +1,12 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :posts
 	has_many :comments
 
-	#########################
-	# Validations
-
-	# TODO
-
-	attr_accessor	:password_confirmation
-
-	#########################
-	# Class Methods
-
-	def self.authenticate( provided_credential, provided_pw )
-		return false unless existing_user = User.find_by_email( provided_credential ) || User.find_by_name( provided_credential.downcase )
-
-		hashed_provided_pw = Digest::SHA256.hexdigest( provided_pw )
-
-		if hashed_provided_pw == existing_user.password_hash
-			return existing_user
-		else
-			return false
-		end
-	end
-
-
-
-	#########################
-	# Instance Methods
-
-	def password=( pw )
-		# rails mass assignment methods (via hashes) just assume accessor methods...
-		return false if pw.blank?
-
-		self.password_hash = Digest::SHA256.hexdigest( pw )
-	end
-
-	def to_s
-		self.name.capitalize
-	end
-
-	def to_i
-		self.id
-	end
-
-	def to_h
-		self.attributes
-	end
-
-	#credentials: me@me.com 123
+	
 
 
 end
